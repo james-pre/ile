781ff310f11d3dd74254ce7382ec24b03ea9bcda
James Prevett <jp@jamespre.dev>
2025-02-27 12:25:12 -0600

    Fix name of eslint rule set

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-	name: 'logic-sim',
+	name: 'ILE',
 	extends: [eslint.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
 	files: ['src/**/*.ts'],
 	languageOptions: {
