d61322b9651f6db75bbd3d4b3d69cb25be5d125c
James Prevett <jp@jamespre.dev>
2025-04-17 02:02:47 -0500

    Add pg setup to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 
+echo -n "Checking for postgres... "
+id -u postgres > /dev/null 2>&1
+HAS_PG_USER=$?
+echo "done."
+
 echo -n "Checking for dependencies... "
 if [ -n "$(command -v dnf)" ]; then
 	echo "using dnf."
@@ -10,6 +15,10 @@ elif [ -n "$(command -v apt)" ]; then
 else
 	echo "No package manager found. Please install nodejs and postgresql-server manually."
 	read -p "(Press enter to continue)"
+fi
+
+if [ $HAS_PG_USER -ne 0 ]; then
+	sudo -u postgres initdb -D /var/lib/pgsql/data
 fi
 
 echo "Please make sure these lines are added to pg_hba.conf (usually in /var/lib/pgsql/data) before the lines containing 'all all':"
