7ba8e14d10d4a926750ed4b39bb79fa7f1ad1239
James Prevett <jp@jamespre.dev>
2025-04-17 02:03:15 -0500

    Grammer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -21,7 +21,7 @@ if [ $HAS_PG_USER -ne 0 ]; then
 	sudo -u postgres initdb -D /var/lib/pgsql/data
 fi
 
-echo "Please make sure these lines are added to pg_hba.conf (usually in /var/lib/pgsql/data) before the lines containing 'all all':"
+echo "Make sure these lines are added to pg_hba.conf (usually in /var/lib/pgsql/data) before the lines containing 'all all':"
 echo -e "local axium axium md5\nhost  axium axium 127.0.0.1/32 md5\nhost  axium axium ::1/128 md5"
 read -p "(Press enter to continue)"
 
